38ab50058e2f31585091ef3eee8ac59ec3603c34
Ali Muksin <ali.muksin0510@gmail.com>
2023-05-26 02:51:36 +0700

    CHANGE : initialize projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+import Button, { SecondaryButton, PrimaryButton } from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Button title="Universal Button" variant="success"/>
+			<Routes>
+				<Route path="/" element={<Button title="My Custom Button" variant="danger"/>}/>
+				<Route path="/secondary" element={<SecondaryButton title="My Custom Button" color="red"/>}/>
+				<Route path="/primary" element={<PrimaryButton title="My Custom Button"/>}/>
+			</Routes>
+		</div>
+	);
 }
 
-export default App;
+export default App
